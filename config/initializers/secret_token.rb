aacbc8845d83958d25d4ddc90e4da749cf5b4efa
Gordon Anderson <gordon.b.anderson@gmail.com>
2011-02-08 09:16:12 +0700

    Rails generated skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RgeoExample::Application.config.secret_token = 'decc63d9803eb60252c7a07201a796e4e3be172b6c886c670415f179093ad0e410de48385c05147836af87a7fbac7b574ce3338fa8bd9d7e5e2b32bd4f3f7b10'
