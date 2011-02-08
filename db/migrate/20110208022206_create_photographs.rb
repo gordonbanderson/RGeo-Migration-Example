6aa7ca47f4ea19b66f320b28dc55d68f68bbf00e
Gordon Anderson <gordon.b.anderson@gmail.com>
2011-02-08 09:27:30 +0700

    Migrations: Added spatial rgeo items to photograph migration

--- db/migrate/20110208022206_create_photographs.rb
+++ db/migrate/20110208022206_create_photographs.rb
@@ -1,7 +1,9 @@
 class CreatePhotographs < ActiveRecord::Migration
   def self.up
-    create_table :photographs do |t|
-
+    create_table :photographs, :options => 'ENGINE=MyISAM' do |t|
+      t.column :latlon, :point, :null => false
+      t.line_string :path
+      t.geometry :shape
       t.timestamps
     end
   end
